5b7e26ea46195278a4244d5ecfe3613641f34e17
yurii.lushchyk <yurii.lushchyk@eleks.com>
2019-12-26 20:24:32 +0200

    Add UserRepositoryTest findAllByIdIn

--- UserService/src/test/java/com/eleks/userservice/repository/UserRepositoryTest.java
+++ UserService/src/test/java/com/eleks/userservice/repository/UserRepositoryTest.java
@@ -9,10 +9,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -68,6 +70,41 @@ class UserRepositoryTest {
     @Test
     void findByEmail_UserWithSuchEmailDoesntExist_ReturnNull() {
         Optional<User> found = repository.findByEmail(user.getEmail());
+        assertFalse(found.isPresent());
+    }
+
+    @Test
+    void findAllByIdIn_SaveTwoUsersAndFindThemByIds_ReturnTwoUsers() {
+        List<User> users = Arrays.asList(
+                User.builder()
+                        .username("username1")
+                        .firstName("firstName1")
+                        .lastName("lastName1")
+                        .dateOfBirth(LocalDate.now())
+                        .email("email1@eleks.com")
+                        .receiveNotifications(false)
+                        .build(),
+                User.builder()
+                        .username("username2")
+                        .firstName("firstName2")
+                        .lastName("lastName2")
+                        .dateOfBirth(LocalDate.now())
+                        .email("email2@eleks.com")
+                        .receiveNotifications(false)
+                        .build());
+        List<User> savedUsers = repository.saveAll(users);
+        List<Long> ids = savedUsers.stream().map(User::getId).collect(Collectors.toList());
+
+        Optional<List<User>> found = repository.findAllByIdIn(ids);
+
+        assertTrue(found.isPresent());
+        assertEquals(users.size(), found.get().size());
+    }
+
+    @Test
+    void findAllByIdIn_NoUsersAndFindByIds_ReturnTwoUsers() {
+        Optional<List<User>> found = repository.findAllByIdIn(Arrays.asList(1L, 2L));
+
         assertFalse(found.isPresent());
     }
 }
