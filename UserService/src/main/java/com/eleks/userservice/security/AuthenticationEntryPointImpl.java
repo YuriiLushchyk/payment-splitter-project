23af3fe86f1920948630081feb61d986afe43a08
yurii.lushchyk <yurii.lushchyk@eleks.com>
2020-01-06 13:36:10 +0200

    Handle BadCredentialsException

--- UserService/src/main/java/com/eleks/userservice/security/AuthenticationEntryPointImpl.java
+++ UserService/src/main/java/com/eleks/userservice/security/AuthenticationEntryPointImpl.java
@@ -1,6 +1,12 @@
 package com.eleks.userservice.security;
 
 
+import com.eleks.userservice.dto.ErrorDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -8,13 +14,37 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Collections;
 
 @Component
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
 
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public AuthenticationEntryPointImpl(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        String msg;
+        if (authException instanceof BadCredentialsException) {
+            msg = authException.getMessage();
+        } else {
+            msg = "Unauthorized";
+        }
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .statusCode(HttpStatus.UNAUTHORIZED.value())
+                .messages(Collections.singletonList(msg))
+                .timestamp(LocalDateTime.now())
+                .build();
+        objectMapper.writeValue(response.getOutputStream(), errorDto);
     }
 }
